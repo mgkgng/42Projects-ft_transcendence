52eca48e41092bae3d103db42ac2905ab583c03d
Minguk Gaang <minguk.gaang@gmail.com>
2022-09-19 17:56:29 +0200

    re, save before animation

--- new_front/src/App.svelte
+++ new_front/src/App.svelte
@@ -24,9 +24,10 @@
 		left: 100px;
 		background-color: $background;
 		margin: 2em;
-		padding: .4em 0 .2em;
+		padding: .3em 0 .1em;
 		font-size: 75px;
 		text-align: center;
+		color: blue;
 	}
 </style>
 
@@ -48,27 +49,25 @@
 
 	function createCircles() {
 		let res = [];
-		let circleNb = Math.floor(Math.random() * 50 + 32);
+		let circleNb = Math.floor(Math.random() * 10 + 32);
 	
 		for (let i = 0; i < circleNb; i++) {
 			let circle = {};
-			let circleInfo = "";
 
 			let angle = Math.floor(Math.random() * 360);
 
 			let distCenterX = circleRadius * Math.sin(Math.PI * 2 * angle / 360);
 			let distCenterY = circleRadius * Math.cos(Math.PI * 2 * angle / 360);
 
-			let size = Math.floor(Math.random() * 15 + 8);
+			let size = Math.floor(Math.random() * 30 + 8);
 
 			circle["x"] = topMargin + circleRadius + distCenterX;
 			circle["y"] = leftMargin + circleRadius + distCenterY;
-			circle["x"] += !(angle > 0 && angle < 180) ? (size + 10) * -1: size;
-			circle["y"] += (angle > 90 && angle < 270) ? (size + 10) * -1: size;
+			circle["x"] += !(angle > 0 && angle < 180) ? (size + 5) * -1: 5;
+			circle["y"] += (angle > 90 && angle < 270) ? (size + 5) * -1: 5;
 			circle["size"] = size;
 			// circle["speed"] = Math.floor(Math.random() * 25 + 20).toString();
 			// circle["rotateDir"] = Math.floor(Math.random() * 2).toString();
-			// circleInfo = makeCssStyleVars(circle);
 			res.push(circle);
 		}
 		return (res)
@@ -84,10 +83,11 @@
 <main>
 	<div class="main-circle" style="top: {topMargin}px; left: {leftMargin}; width: {circleRadius * 2}px"></div>
 
+
+	<h1 class="title">ft_transcendence</h1>
 	{#each circlesAround as circleInfo}
 	<div class="circle-around" style="--startX: {circleInfo.x}px; --startY: {circleInfo.y}px; --size: {circleInfo.size}px;"></div>
 	{/each}
-	<h1 class="title">ft_transcendence</h1>
 	
 </main>
 
