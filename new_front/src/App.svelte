905577f691bb593e2f0ff5893c04e120e60fca48
Minguk Gaang <minguk.gaang@gmail.com>
2022-09-19 14:15:43 +0200

    save lunch. createCricles func for front + started to think about pong.svelte

--- new_front/src/App.svelte
+++ new_front/src/App.svelte
@@ -22,11 +22,34 @@
 </style>
 
 <script lang="ts">
+	import { onMount } from "svelte/types/runtime/internal/lifecycle";
 
+	let circlesAround;
+
+	function createCircles() {
+		let circleInfos = [];
+		let circleNb = Math.floor(Math.random() * 16 + 12);
+		
+		for (let i = 0; i < circleNb; i++) {
+			let circle = {};
+			// circle["startX"] =
+			// circle["startY"] = 
+			circle["size"] = Math.floor(Math.random() * 15 + 8);
+			circle["speed"] = Math.floor(Math.random() * 25 + 20);
+			circle["direction"] = Math.floor(Math.random() * 2);
+			circleInfos.push(circle);
+		}
+	}
+
+	onMount(() => {
+		createCircles();
+	});
+	
 </script>
 
 <main>
 	<div class="main-circle"></div>
+	
 	<h1 class="title">ft_transcendence</h1>
 </main>
 
--- new_front/src/Pong.svelte
+++ new_front/src/Pong.svelte
@@ -1,10 +1,15 @@
-<style>
+<style lang="scss">
 	.bar {
-
+		width: 10px;
+		height: 3px;
+		background-color: $bar;
 	}
 </style>
 
 <script>
+	function handleKey(e) {
+		console.log(e);
+	}	
 </script>
 
-<div class="bar"></div>
+<div class="bar" on:keydown={handleKey}></div>
