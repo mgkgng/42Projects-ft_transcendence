dba6bf0d65f077defbdd2c3066efb2f7ca114425
Le H <hadufer@student.42.fr>
2022-10-06 18:27:30 +0200

    User Friend entity realtionship added in database

---
+++ back/src/entity/UserFriend.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { UserEntity } from "./User.entity";
+
+@Entity()
+export class UserFriendEntity {
+    @PrimaryGeneratedColumn()
+	id_g: number;
+    
+    @ManyToOne(() => UserEntity, (user: UserEntity) => user.relation_friend_requested)
+	id_first_user: UserEntity; // Les User qui mont demander en amis
+
+	@ManyToOne(() => UserEntity, (user: UserEntity) => user.relation_friend_accepted)
+	id_second_user: UserEntity; // les User que j'ai accepter en amis
+    
+    @Column({default: false})
+    is_user_friend?: boolean;
+}
--- back/src/user/user.module.ts
+++ back/src/user/user.module.ts
@@ -9,10 +9,11 @@ import { GameEntity } from 'src/entity/Game.entity';
 import { MessageChatRoomEntity } from 'src/entity/MessageChatRoom.entity';
 import { MessageDirectEntity } from 'src/entity/MessageDirect.entity';
 import { UserBlockEntity } from 'src/entity/UserBlock.entity';
+import { UserFriendEntity } from 'src/entity/UserFriend.entity';
 
 @Module({
 
-	imports :	[TypeOrmModule.forFeature([UserEntity, UserChatRoomEntity, ChatRoomEntity, GameEntity, MessageChatRoomEntity, MessageDirectEntity, UserBlockEntity])],
+	imports :	[TypeOrmModule.forFeature([UserEntity, UserChatRoomEntity, ChatRoomEntity, GameEntity, MessageChatRoomEntity, MessageDirectEntity, UserBlockEntity, UserFriendEntity])],
   controllers: [UserController],
   providers: [UserService],
   exports : [UserService]
