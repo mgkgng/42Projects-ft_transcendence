4bdedcd3df180ee064d8e2d92df81e93adc7310b
Minguk Gaang <minguk.gaang@gmail.com>
2022-10-11 13:56:23 +0200

    vue init

---
+++ vue/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="test">
+		TEST TEST
+	</div>
+</template>
+
+<style>
+
+.test{
+	width: 1000px;
+	height: 500px;
+	background-color: rgb(191, 191, 253);
+}
+.nuxt-logo {
+  height: 180px;
+}
+</style>
