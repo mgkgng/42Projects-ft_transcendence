b9742f2ee2f2822fe3908e8522e032a1f22ec0c1
Minguk Gaang <minguk.gaang@gmail.com>
2022-10-11 21:12:39 +0200

    re

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,33 +1,33 @@
 import { Controller, Post, Get, UseGuards, Request } from '@nestjs/common';
-// import { AuthGuard } from '@nestjs/passport';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
-// import { AuthService } from './auth/auth.service';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  // constructor(private readonly appService: AppService,
-  //             private authService : AuthService) {}
-  constructor(private readonly appService: AppService){}
+  constructor(private readonly appService: AppService,
+              private authService : AuthService) {}
+  // constructor(private readonly appService: AppService){}
 
-  //@Get("/")
-  //getHello(): string {
-    //return ("Je te baise");
-  //}
-  // @UseGuards(AuthGuard('jwt'))
-  // @Get()
-  // test(): string {
-  //   return "Hello World";
-  // } 
-  // @UseGuards(AuthGuard('local'))
-  // @Post('/auth')
-  // async  login(@Request() req)
-  // {
-  //     return (this.authService.login(req.user));
-  // }
-  // @UseGuards(AuthGuard('oauth'))
-  // @Post("/auth42")
-  // async login42(@Request() req)
-  // {
-  //     return (this.authService.login(req.user));
-  // }
+  @Get("/")
+  getHello(): string {
+    return ("Je te baise");
+  }
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  test(): string {
+    return "Hello World";
+  } 
+  @UseGuards(AuthGuard('local'))
+  @Post('/auth')
+  async  login(@Request() req)
+  {
+      return (this.authService.login(req.user));
+  }
+  @UseGuards(AuthGuard('oauth'))
+  @Post("/auth42")
+  async login42(@Request() req)
+  {
+      return (this.authService.login(req.user));
+  }
 }
