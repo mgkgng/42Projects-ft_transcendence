65c0ac074fb61bfdcc5a558b73f79028a595f77a
Le H <hadufer@student.42.fr>
2022-10-19 17:13:18 +0200

    askFriend function added working properly (need to guard and protect from sql injection)

--- back/src/friendSystem/friendSystem.controller.ts
+++ back/src/friendSystem/friendSystem.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Query } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { parse } from "path";
+import { first } from "rxjs";
 import { UserEntity } from "src/entity/User.entity";
 import { UserFriendEntity } from "src/entity/UserFriend.entity";
 import { Repository } from "typeorm";
@@ -54,12 +56,11 @@ export class friendSystemController {
         return await this.userRepository.save(user);
     }
 
+    // Do a request on this route to ask if 2 user are friend
     @Get("isfriendwith?")
     async isFriendWithByUsername(@Query() query : {first_username : string, second_username : string})
     {
         const qb = this.userFriendRepository.createQueryBuilder('u');
-        console.log(query.first_username)
-        console.log(query.second_username)
         let first_username = query.first_username;
         let second_username = query.second_username;
         return await qb
@@ -68,11 +69,70 @@ export class friendSystemController {
         .where(`friendrequester.username = '${first_username}' OR friendrequested.username = '${second_username}'`)
         .andWhere(`friendrequester.username = '${second_username}' OR friendrequested.username = '${first_username}'`)
         .andWhere(`u.is_user_friend = true`)
-        // .getSql();
-        .getMany();
+        .getOne();
     }
 
-    // Friend Request
+    // Do a request to this route to ask for the friendList of a user
+    @Get("getFriendList?")
+    async getFriendList(@Query() query : {username : string })
+    {
+        const qb = this.userFriendRepository.createQueryBuilder('u');
+        console.log(query.username)
+        let username = query.username;
+        let unparsedQuery = await qb
+        .leftJoinAndSelect("u.id_first_user", "friendrequester")
+        .leftJoinAndSelect("u.id_second_user", "friendrequested")
+        .where(`friendrequester.username = '${username}' OR friendrequested.username = '${username}'`)
+        .andWhere(`u.is_user_friend = true`)
+        .getMany();
+
+        let i : number = 0;
+        console.log(unparsedQuery)
+        let parsedList = [];
+        unparsedQuery.forEach(element => {
+            if (element.id_first_user.username == username)
+                parsedList.push(element.id_second_user);
+            if (element.id_second_user.username == username)
+                parsedList.push(element.id_first_user);
+            i++;
+        });
+        return parsedList;
+    }
+
+    // If first_username have already asked second_username no request is send
+    // If second_username have already asked first_username the field is_user_friend
+    // of the request from second_username become true.
+    @Get("askfriend?")
+    async askFriend(@Query() query : {first_username : string, second_username : string})
+    {
+        const qbu = this.userRepository.createQueryBuilder('u');
+        const qb = this.userFriendRepository.createQueryBuilder('u');
+        let first_username = query.first_username;
+        let second_username = query.second_username;
+        let firstHaveAlreadyBeenRequestEntity = await qb.leftJoinAndSelect("u.id_first_user", "friendrequester")
+        .leftJoinAndSelect("u.id_second_user", "friendrequested")
+        .where(`friendrequester.username = '${second_username}' AND friendrequested.username = '${first_username}'`)
+        .getOne();
+        let firstHaveAlreadyRequestedEntity = await qb
+        .where(`friendrequester.username = '${first_username}' AND friendrequested.username = '${second_username}'`)
+        .getOne();
+        if (firstHaveAlreadyBeenRequestEntity)
+        {
+            console.log("i exist")
+            firstHaveAlreadyBeenRequestEntity.is_user_friend = true;
+            return await this.userFriendRepository.save(firstHaveAlreadyBeenRequestEntity);   
+        }
+        else if (firstHaveAlreadyRequestedEntity == null)
+        {
+            console.log("i doesnt exist")
+            const newUserRel = new UserFriendEntity();
+            newUserRel.id_first_user = await qbu.select().where(`u.username = '${first_username}'`).getOneOrFail();
+            newUserRel.id_second_user = await qbu.select().where(`u.username = '${second_username}'`).getOneOrFail();
+            newUserRel.is_user_friend = false;
+            return await this.userFriendRepository.save(newUserRel);
+        }
+        return firstHaveAlreadyRequestedEntity;
+    }
 
     // Accept Friend
 
