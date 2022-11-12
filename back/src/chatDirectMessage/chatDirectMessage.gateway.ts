e99933eb9338780e590328d450847fec39eecfb5
Le H <hadufer@student.42.fr>
2022-11-12 08:23:54 +0100

    [chatDirectMessage] If user a send message to user b and user have blocked user a then the message will be stored in db but will not be send with websocket to the user b

--- back/src/chatDirectMessage/chatDirectMessage.gateway.ts
+++ back/src/chatDirectMessage/chatDirectMessage.gateway.ts
@@ -1,7 +1,10 @@
 import { Inject } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
 import { MainServerService } from "src/mainServer/mainServer.gateway";
 import { ChatDirectMessageService } from "./chatDirectMessage.service";
+import { Repository } from "typeorm";
+import { UserEntity } from "src/entity/User.entity";
 
 @WebSocketGateway({
 	cors: {
@@ -13,16 +16,19 @@ export class ChatDirectMessageGateway {
         @Inject(MainServerService)
         private mainServerService : MainServerService,
         @Inject(ChatDirectMessageService)
-        private chatDirectMessageService : ChatDirectMessageService
+        private chatDirectMessageService : ChatDirectMessageService,
+        @InjectRepository(UserEntity)
+        private userRepository : Repository<UserEntity>
     ) {}
 
     // Subscribe to the event 'receiveMessage' from the client
     // and call the method 'receiveMessage' of the service
     @SubscribeMessage('newDirectMessage')
     async receiveMessage(@MessageBody() data: any, @ConnectedSocket() client: any) {
+        const user = await this.userRepository.findOne({where: {username: data.username}, relations: ['relation_userBlocked']});
         const userConnected = this.mainServerService.getUserConnectedByUsername(data.username);
-        if (userConnected)
+        if (userConnected && user.relation_userBlocked.includes(client.username) == false)
             userConnected.socket.emit('newDirectMessage', data.message);
-        // return this.chatDirectMessageService.handleSendDirectMessage(data.username_sender, data.username_receiver, data.message);
+        // this.chatDirectMessageService.handleSendDirectMessage(this.mainServerService.getUserConnectedBySocketId(client.id).username, data.username, data.message);
     }
 }
