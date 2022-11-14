af42c5c8962d4aa08e955f653289fdd35cdec678
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-14 13:30:05 +0100

    friendlist interface update

--- front/src/lib/modals/Friends.svelte
+++ front/src/lib/modals/Friends.svelte
@@ -1,30 +1,67 @@
 <style lang="scss">
 	.friends {
 		width: 360px;
-		height: 540px;
+		height: 480px;
+		padding-bottom: .2em;
+		padding-right: .5em;
+		justify-content: space-between;
 
 		.friends-list {
-			padding-top: 2em;
-			padding-left: .5em;
-			gap: 1em;
-			overflow: scroll;
+			margin-top: 1em;
+			padding-left: .2em;
+			gap: .8em;
+			overflow-y: scroll;
 			display: flex;
 			flex-direction: column;
-
 
 			width: 100%;
 			height: 80%;
 			border: none;
 
-			.friend {
-				width: 60%;
-				cursor: pointer;
-				border-radius: .2em .2em 3em .2em;
-				transition: .3s;
-				padding: 1em;
+			.line {
+				position: relative;
+				display: flex;
+				flex-direction: row;
+				gap: 0;
 
-				&:hover {
-					background-color: transparentize(#fff, .6);
+				.friend {
+					width: 60%;
+					cursor: pointer;
+					border-radius: .2em .2em 3em .2em;
+					transition: .3s;
+					padding: 1em;
+
+					&:hover {
+						background-color: transparentize($main-light, .3);
+						filter: saturate(50%);
+					}
+				}
+				.tools {
+					position: absolute;
+					right: 2em;
+					width: 7em;
+					float: right;
+					display: flex;
+					flex-direction: row;
+					justify-content: flex-end;
+					gap: 2em;
+
+					padding: 1em;
+					border-radius: 3em .2em .2em .2em;
+					cursor: pointer;
+
+					img {
+						height: 1.2em;
+					}
+
+					button {
+						cursor: pointer;
+					}
+
+					&:hover {
+						background-color: transparentize($submain-lowshadeblue, .3);
+						filter: saturate(50%);
+					}
 				}
 			}
 		}
@@ -63,7 +100,13 @@
 	{#if friends}
 	<div class="friends-list">
 		{#each friends as friend}
-		<div class="friend">{friend.username}</div>
+		<div class="line">
+			<div class="friend">{friend.username}</div>
+			<div class="tools">
+				<button><img src="/icon-mail.png" alt="mail"></button>
+				<button>-</button>
+			</div>
+		</div>
 		{/each}
 	</div>
 	{:else}
