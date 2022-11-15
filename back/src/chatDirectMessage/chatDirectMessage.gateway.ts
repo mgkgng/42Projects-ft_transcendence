cc9f9efd1f8c95c9e5906b67e6d58daf434d2f04
Hassan Dufer <hadufer@c4r2p3.42nice.fr>
2022-11-15 16:08:50 +0100

    new Message system

--- back/src/chatDirectMessage/chatDirectMessage.gateway.ts
+++ back/src/chatDirectMessage/chatDirectMessage.gateway.ts
@@ -5,6 +5,7 @@ import { MainServerService } from "src/mainServer/mainServer.gateway";
 import { ChatDirectMessageService } from "./chatDirectMessage.service";
 import { Repository } from "typeorm";
 import { UserEntity } from "src/entity/User.entity";
+import { WebSocketServer } from "@nestjs/websockets";
 
 @WebSocketGateway({
 	cors: {
@@ -21,6 +22,8 @@ export class ChatDirectMessageGateway {
         private userRepository : Repository<UserEntity>
     ) {}
 
+	@WebSocketServer() server;
+
 
     @SubscribeMessage('newDirectMessage')
     async receiveMessage(@MessageBody() data: any, @ConnectedSocket() client: any) {
@@ -33,4 +36,29 @@ export class ChatDirectMessageGateway {
             userConnected.socket.emit('newDirectMessage', data.message);
         let ret = await this.chatDirectMessageService.handleSendDirectMessage(this.mainServerService.getUserConnectedBySocketId(client.id).username, data.username, data.message);
     }
+
+	@SubscribeMessage('sendDirectMessage')
+	async sendMessage(@MessageBody() data: any, @ConnectedSocket() client: any) {
+		const user = await this.userRepository.findOne({
+			where: {username: data.username},
+			relations: ['relation_userBlocked']
+		});
+		const userSender = await this.userRepository.findOne({
+			where: {username: this.mainServerService.getUserConnectedBySocketId(client.id).username},
+			relations: ['relation_userBlocked']
+		});
+		if (!user || !userSender || userSender.relation_userBlocked.includes(data.username))
+		{
+			this.server.to(client.id).emit('error_sendDirectMessage', {error: 'User not found or blocked'});
+			return;
+		}
+		const userConnected = this.mainServerService.getUserConnectedByUsername(data.username);
+		if (userConnected && user.relation_userBlocked.includes(client.username) == false)
+			userConnected.socket.emit('getDirectMessage', {sender: userSender.username, message: data.message});
+		let ret = await this.chatDirectMessageService.handleSendDirectMessage(this.mainServerService.getUserConnectedBySocketId(client.id).username, data.username, data.message);
+		if (ret)
+			this.server.to(client.id).emit('success_sendDirectMessage', {message: data.message});
+		else
+			this.server.to(client.id).emit('error_sendDirectMessage', {error: 'An error occured'});
+	}
 }
