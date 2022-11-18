804865ed30957e05067c5a402ea1e9201c078c47
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-18 11:46:25 +0100

    refresh + logout error debugged

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -22,6 +22,8 @@
 	import io, { Socket } from "socket.io-client";
     import { user } from '$lib/stores/user';
     import { goto } from '$app/navigation';
+	import jwt_decode from "jwt-decode";
+
 
 	let roomModal: any;
 	let roomId: string = "";
@@ -73,7 +75,7 @@
 			});
 			localStorage.setItem('transcendence-jwt', tok.access_token);
 			$client.connect();
-			const val : any = await jwt_decode(localStorage.getItem("transcendence-jwt"));
+			const val : any = await jwt_decode(tok.access_token);
 			loginState.set(true);
 			goto('/');
 		}catch(e){
@@ -90,53 +92,60 @@
 
 		console.log("yo?");
 
-		if (localStorage.getItem('transcendence-jwt') != null
-		&& localStorage.getItem('transcendence-jwt') != undefined)
-		{
-			const tok = localStorage.getItem('transcendence-jwt');
+		
+
+		if (!$client.socket) {
+			if (localStorage.getItem('transcendence-jwt') != null
+				&& localStorage.getItem('transcendence-jwt') != undefined)
 			{
-				$client.socket = await io("http://localhost:3001",{
-					extraHeaders: {
-						Authorization: "Bearer " + tok,
-					},
-					autoConnect: false,
-				},);
-				await $client.socket.connect();
-				console.log($client.socket);
+				console.log("came here");
+				const tok = localStorage.getItem('transcendence-jwt');
+				{
+					$client.socket = await io("http://localhost:3001",{
+						extraHeaders: {
+							Authorization: "Bearer " + tok,
+						},
+						autoConnect: false,
+					},);
+					await $client.socket.connect();
+					console.log($client.socket);
+				}
 			}
-		}
 
-		let url = new URLSearchParams(window.location.search);
-		if ((!$client.socket || !$client.socket.connected) && url.has('code'))
-			await connectWithUrlCode(url);
+			let url = new URLSearchParams(window.location.search);
+			if ((!$client.socket || !$client.socket.connected) && url.has('code'))
+				await connectWithUrlCode(url);
 
-		if ($client.socket) {
-			$client.socket.on("GetConnectionInfo", (data: any) => {
-				console.log("GetConnectionInfo", data);
-				$client.id = data.id;
-				user.set(data.user);				
-			});	
+			if ($client.socket) {
+				$client.socket.on("GetConnectionInfo", (data: any) => {
+					console.log("GetConnectionInfo", data);
+					$client.id = data.id;
+					user.set(data.user);				
+				});	
 
-			$client.socket.on("connection", (data: any) => {
-				console.log("connection", data);
-				$client.connect();
-				loginState.set(true);
-				// $chatRoom.LoadMessages($client);
-			});
-			$client.socket.on("get_user_info", (data: any) => {
-				client.update((value) => {
-					value.username = data.username;
-					value.user_info = data;
-					return value;
+				$client.socket.on("connection", (data: any) => {
+					console.log("connection", data);
+					$client.connect();
+					loginState.set(true);
+					// $chatRoom.LoadMessages($client);
 				});
-				console.log("DATA: ", data);
-			});
-			$client.socket.emit("get_user_info", {});
-		}
-		goto('/');
+				$client.socket.on("get_user_info", (data: any) => {
+					client.update((value) => {
+						value.username = data.username;
+						value.user_info = data;
+						return value;
+					});
+					console.log("DATA: ", data);
+				});
+				$client.socket.emit("get_user_info", {});
+			}
 
-		if (!$client.socket)
-			return;
+			console.log("here", $client);
+			if ($client.socket)
+				goto('/');
+			else
+				return ;
+		}
 
 		$client.socket.on("RoomCreated", (data: any) => {
 			console.log("RoomCreated", data);
