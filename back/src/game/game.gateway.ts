94a3984b52b22e519ad4cb24d82450c8653b6157
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-18 17:46:01 +0100

    history update

--- back/src/game/game.gateway.ts
+++ back/src/game/game.gateway.ts
@@ -307,15 +307,16 @@ export class GameGateway {
 		let room = this.getRoom(data.roomId);
 		client.emit("getChatGameMessage", room.chat);	
 	} 
-	@SubscribeMessage("getHistoryGame")
+	@SubscribeMessage("getHistory")
 	async getHistGame(@MessageBody() data: any, @ConnectedSocket() client: Socket, @Request() req) {
 		let id_user = await this.mainServerService.getIdUser(req);
 		const res = await this.dataSource.getRepository(GameEntity).createQueryBuilder("game")
-		.innerJoin("game.player1", "user")
+		.innerJoin("game.player1", "user1")
 		.innerJoin("game.player2", "user2")
 		.where("game.player1.id_g = :u or game.player2.id_g = :u", {u: id_user})
-		.select(["game.player1_score", "game.player2_score", "user.username", "user2.username", "game.date_game"]).getMany();
-		client.emit("getHistoryGame", res);
+		.select(["game.player1_score", "game.player2_score", "user1.username", "user2.username", "user1.img_url", "user1.img", "user2.img_url", "user2.img", "game.date_game"]).getMany();
+		
+		client.emit("resHistory", res);
 	}
 
 	getClient(id: string) { return (this.clients.get(id)); }
