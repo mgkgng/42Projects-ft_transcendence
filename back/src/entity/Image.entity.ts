3fc2be3afb771061fabd4011038a0539d4d465de
Le H <hadufer@student.42.fr>
2022-11-21 15:14:54 +0100

    Add of image upload

--- back/src/entity/Image.entity.ts
+++ back/src/entity/Image.entity.ts
@@ -1,15 +1,26 @@
 import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity } from 'typeorm';
 
+@Entity()
 export class ImageEntity {
 	@PrimaryGeneratedColumn()
 	id_g: number;
 
 	@Column()
+	img_uid: string;
+
+	@Column()
 	img_url: string;
 
 	@Column()
-	img_height: number;
+	img_size: number;
 
 	@Column()
-	img_width: number;
+	img_name: string;
+
+	@Column()
+	img_type: string;
+
+	@Column()
+	img_data: string;
 }
--- back/src/image/image.service.ts
+++ back/src/image/image.service.ts
@@ -2,14 +2,30 @@ import { Repository } from 'typeorm';
 import { ImageEntity } from 'src/entity/Image.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entity/User.entity';
 
 @Injectable()
 export class ImageService {
     constructor(
 		@InjectRepository(ImageEntity)
         private imageRepository : Repository<ImageEntity>,
+        @InjectRepository(UserEntity)
+        private userRepository : Repository<UserEntity>
     ){}
-    async uploadImage(image : ImageEntity) {
+
+    async saveImage(image : ImageEntity) {
         return await this.imageRepository.save(image);
+    }
+
+    async getImage(uid : string) {
+        return await this.imageRepository.findOne({where:{img_uid : uid}});
+    }
+
+    async changeUserImage(username : string, image : ImageEntity) {
+        const user = await this.userRepository.findOne({where:{username : username}});
+        if (!user)
+            return null;
+        user.img_url = image.img_url;
+        return await this.userRepository.save(user);
     }
 }
