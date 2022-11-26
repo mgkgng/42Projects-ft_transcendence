c0a5ed0eb1d14bd8801e8ff3f3cd29299616e971
Minguk Gaang <minguk.gaang@gmail.com>
2022-11-26 13:07:53 +0100

    change username front done

--- front/src/lib/settings/ChangeUsername.svelte
+++ front/src/lib/settings/ChangeUsername.svelte
@@ -1,36 +1,29 @@
 <style lang="scss">
 	.modify {
 		width: 18em;
-		height: 9em;
-		padding: 1em;
-		padding-left: 1.5em;
-		padding-top: 2.5em;
+		height: 12em;
+		justify-content: space-around;
+		align-items: center;
+		
+		gap: .2em;
 
 		.message {
-			position: absolute;
-			top: .6em;
-			left: 20%;
-			color: $red;
-			// make font bold
+			height: 2em;
+			color: #fff;
+			text-align: center;
 		}
-		.input {
+		.err { color: $red; }
+
+		input {
 			width: 100%;
-			height: 40%;
-			gap: .5em;
-		
-			input {
-				width: 13.3em;
-				background-color: #fff;
-				padding-left: .5em;
-				border-radius: .2em;
-				color: #000;
-			}
-			
+			height: 2em;
+			background-color: #fff;
+			padding-left: .5em;
+			border-radius: .2em;
+			color: #000;
 		}
+			
 		.buttons {
-			position: absolute;
-			left: 4em;
-			bottom: 1em;
 			gap: .2em;
 	
 			button {
@@ -60,16 +53,15 @@
 	$: modified = !(username == now);
 	$: checkNewUsername(now);
 
-	let message: string = "";
+	let message: string = "Try your new username";
 	let err: boolean = false;
 
 	function checkNewUsername(now: string) {
-		console.log("???");
 		if (!modified) {
-			message = "";
+			message = "Try your new username";
 			return ;
 		} else if (now.length < 6 || now.length > 15) {
-			message = "Your ID should have have between 6 and 15 characters.";
+			message = "Your ID should have between 6 and 15 characters.";
 			err = true;
 			return ;
 		}
@@ -95,24 +87,18 @@
 	});
 </script>
 
-<div class="flex window modify">
-	{#if message}
+<div class="vflex window modify">
 	<div class="message">
-		{message}
+		{#if message}
+		<p class="{(err) ? "err" : ""}">{message}</p>
+		{/if}
 	</div>
-	{/if}
-	<div class="flex input">
-		<input type="text-input" placeholder="Put your new username here" bind:value={now}>
-		<button style="display: {(modified) ? "block" : "none"}" on:click={() => {
-			message = "";
-			// $client.socket.emit("change_username", newUsername);
-		}}>Try</button>
-	</div>
+	<input type="text-input" placeholder="Put your new username here" bind:value={now}>
 	<div class="flex buttons">
 		<button on:click={() => {
 			itself.close();
 		}}>Cancel</button>
-		<button class="{(modified && confirmed) ? "" : "no-active"}" on:click={() => {
+		<button class="{(modified && !err) ? "" : "no-active"}" on:click={() => {
 			username = now;
 			itself.close();
 		}}>Select</button>
